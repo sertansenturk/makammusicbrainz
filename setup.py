712ff07b97fb111e036229f314bd0d7955094551
Sertan Senturk <contact@sertansenturk.com>
2016-04-07 15:50:51 +0200

    Fixed minor PEP8 violations in setup.yp

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup
 
 setup(name='makammusicbrainz',
       version='1.1',
-      description='Tools fetch metadata related to the CompMusic makam corpus from MusicBrainz',
+      description='Tools fetch metadata related to the CompMusic makam corpus '
+                  'from MusicBrainz',
       author='Sertan Senturk',
       author_email='contact AT sertansenturk DOT com',
       license='agpl 3.0',
       url='http://sertansenturk.com',
       packages=['makammusicbrainz'],
       include_package_data=True,
-)
+      )
