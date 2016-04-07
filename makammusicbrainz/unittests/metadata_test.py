a45fa17a8924451629e510e1f795268aa98ff706
Sertan Senturk <contact@sertansenturk.com>
2016-04-07 16:46:31 +0200

    Added unittests

---
+++ makammusicbrainz/unittests/metadata_test.py
@@ -0,0 +1,52 @@
+from makammusicbrainz.AudioMetadata import AudioMetadata
+from makammusicbrainz.WorkMetadata import WorkMetadata
+import json
+import os
+
+_curr_folder = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_audio_metadata():
+    # get the recording metadata
+    audioMetadata = AudioMetadata(print_warnings=True)
+    mp3file = os.path.join(_curr_folder, '..', '..', 'sampledata',
+                           'huzzam_fasil.mp3')
+    audio_meta = audioMetadata.from_musicbrainz(
+        mp3file, get_work_attributes=True)
+
+    # load the metadata computed earlier
+    saved_meta = _get_saved_audio_meta()
+
+    # remove paths since they are both relative to something else
+    audio_meta.pop("path", None)
+    saved_meta.pop("path", None)
+
+    assert audio_meta == saved_meta
+
+
+def test_work_metadata():
+    # load the audio metadata for the work mbids
+    audio_meta = _get_saved_audio_meta()
+
+    # get the work metadata
+    workMetadata = WorkMetadata(print_warnings=True)
+    work_meta = []
+    for w in audio_meta['works']:
+        work_meta.append(workMetadata.from_musicbrainz(w['mbid']))
+
+    # load the metadata computed earlier
+    saved_meta = _get_saved_work_meta()
+
+    assert work_meta == saved_meta
+
+
+def _get_saved_audio_meta():
+    saved_meta_file = os.path.join(_curr_folder, 'audio_meta.json')
+    saved_meta = json.load(open(saved_meta_file, 'r'))
+    return saved_meta
+
+
+def _get_saved_work_meta():
+    saved_meta_file = os.path.join(_curr_folder, 'work_meta.json')
+    saved_meta = json.load(open(saved_meta_file, 'r'))
+    return saved_meta
