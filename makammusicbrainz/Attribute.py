f3550e56f06a4e1b74eb23b46a2a7ae9d92b8650
Sertan Senturk <contact@sertansenturk.com>
2016-04-08 03:54:21 +0200

    Modularized Attribute.get_attrib_tags

--- makammusicbrainz/Attribute.py
+++ makammusicbrainz/Attribute.py
@@ -4,21 +4,21 @@ import os
 
 class Attribute(object):
     @staticmethod
-    def get_attrib_dict(attrstr):
+    def _get_attrib_dict(attrstr):
         attrfile = os.path.join(os.path.dirname(
             os.path.abspath(__file__)), 'makam_data', attrstr + '.json')
         return json.load(open(attrfile, 'r'))
 
     @staticmethod
     def get_attr_key_from_mb_attr(attr_str, attr_type):
-        attr_dict = Attribute.get_attrib_dict(attr_type)
+        attr_dict = Attribute._get_attrib_dict(attr_type)
         for attr_key, attr_val in attr_dict.iteritems():
             if attr_val['dunya_name'] == attr_str:
                 return attr_key
 
     @staticmethod
-    def get_attr_key_from_mb_tag(attr_str, attr_type):
-        attr_dict = Attribute.get_attrib_dict(attr_type)
+    def _get_attr_key_from_mb_tag(attr_str, attr_type):
+        attr_dict = Attribute._get_attrib_dict(attr_type)
         for attr_key, attr_val in attr_dict.iteritems():
             if attr_str in attr_val['mb_tag']:
                 return attr_key
@@ -28,17 +28,20 @@ class Attribute(object):
         theory_attribute_keys = ['makam', 'form', 'usul']
         attributes = dict()
         if 'tag-list' in meta.keys():
-            for k in theory_attribute_keys:
-                for t in meta['tag-list']:  # no work get attrs from the tags
-                    try:
-                        key, val = t['name'].split(': ')
-                        if k in key:
-                            if k not in attributes.keys():
-                                attributes[k] = []
-                            attributes[k].append(
-                                {'mb_tag': val, 'attribute_key':
-                                    Attribute.get_attr_key_from_mb_tag(
-                                        val, k)})
+            for k in theory_attribute_keys:  # for makam/form/usul keys
+                for t in meta['tag-list']:  # for each tag
+                    try:  # attempt to assign the tag to the attribute key
+                        Attribute._assign_attrib(attributes, k, t)
                     except ValueError:
-                        pass  # skip
+                        pass  # not a makam/form/usul tag; skip
         return attributes
+
+    @staticmethod
+    def _assign_attrib(attributes, k, t):
+        key, val = t['name'].split(': ')
+        if k in key:
+            if k not in attributes.keys():  # create the key
+                attributes[k] = []
+
+            attributes[k].append({'mb_tag': val, 'attribute_key':
+                Attribute._get_attr_key_from_mb_tag(val, k)})
