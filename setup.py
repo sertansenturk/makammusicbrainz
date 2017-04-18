2cd2fa3192cc7387f6ecffb9e4546f5e02ec1151
Sertan Şentürk <contact@sertansenturk.com>
2017-04-18 21:52:30 +0200

    Update setup.py
    incrementing the version to 1.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='makammusicbrainz',
-      version='1.4.0',
+      version='1.4.1',
       description='Tools fetch metadata related to the CompMusic makam corpus '
                   'from MusicBrainz',
       author='Sertan Senturk',
